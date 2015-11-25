1d28dacd7ed58320c646e7867ff300837b87cfc6
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-11-25 16:28:07 -0500

    added framework target

---
+++ Framework/FormatterKit/FormatterKit/FormatterKit.h
@@ -0,0 +1,26 @@
+//
+//  FormatterKit.h
+//  FormatterKit
+//
+//  Created by Alsey Coleman Miller on 11/25/15.
+//  Copyright © 2015 mattt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FormatterKit.
+FOUNDATION_EXPORT double FormatterKitVersionNumber;
+
+//! Project version string for FormatterKit.
+FOUNDATION_EXPORT const unsigned char FormatterKitVersionString[];
+
+#import <FormatterKit/TTTAddressFormatter.h>
+#import <FormatterKit/TTTArrayFormatter.h>
+#import <FormatterKit/TTTColorFormatter.h>
+#import <FormatterKit/TTTLocationFormatter.h>
+#import <FormatterKit/TTTNameFormatter.h>
+#import <FormatterKit/TTTOrdinalNumberFormatter.h>
+#import <FormatterKit/TTTTimeIntervalFormatter>
+#import <FormatterKit/TTTUnitOfInformationFormatter.h>
+#import <FormatterKit/TTTURLRequestFormatter>
+
